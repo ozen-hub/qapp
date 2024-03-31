7a1ff561796e8116a28af7f91aec5fdfa0e6776a
ozen-hub <info.ozenhub@gmail.com>
2024-03-31 22:58:34 +0530

    init

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 let interval=undefined;
 let min=0;
 let sec=0;
-let questionNumber=1;
+let questionNumber=0;
 let count=0;
+let op=['+','-','*','/','%'];
+
+let qNum1=0;
+let qNum2=0;
+let selectedOp='';
 
 let minElement=document.getElementById('min');
 let secElement=document.getElementById('sec');
 
+let countElement=document.getElementById('count');
 
+const setCount=()=>{
+    count++;
+    countElement.innerHTML=count;
+}
+
+const manageQuestion=()=>{
+    
+}
 
 const reset=()=>{
     if(interval){
